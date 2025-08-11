aca449a6adf219753bc6c54533ba697e2ef5e947
Cailee <152091809+caileeaverill@users.noreply.github.com>
2025-08-11 14:10:27 -0400

    Add initial implementation for band name generator

---
+++ day-1-working-with-variables/main.py
@@ -0,0 +1,4 @@
+city_name = input("What is the name of the city you grew up in?\n")
+childhood_pet = input("What animal was your childhood pet?\n")
+print(f"Your band name is {city_name} {childhood_pet} ")
+# f (formatted string) is like JS string interpolation instead of a backtick (`)
