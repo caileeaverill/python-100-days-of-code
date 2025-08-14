8a12081498c13e1da2166cb611984e6f87a1c197
Cailee <caileeaverill@gmail.com>
2025-08-14 15:44:49 -0400

    Add PyPassword Generator script
    Introduces a Python script that generates a random password based on user-specified counts of letters, symbols, and numbers. The script prompts the user for input and outputs the generated password.

---
+++ day-5-loops/main.py
@@ -0,0 +1,77 @@
+import random
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+print("Welcome to the PyPassword Generator!")
+
+num_letters = int(input("How many letters? \n"))
+num_symbols = int(input("How many symbols? \n"))
+num_nums = int(input("How many numbers? \n"))
+
+password = ""
+
+for _ in range(num_letters):
+    password += random.choice(letters)
+
+for _ in range(num_symbols):
+    password += random.choice(symbols)
+
+for _ in range(num_nums):
+    password += random.choice(numbers)
+
+print(f"Generated password is: {password}")
